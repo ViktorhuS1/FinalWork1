bfbf9ce98fb3b56f39620c111e3c0fd1239271e4
Viktor Sukhorebrik <viktorsuh1999@gmail.com>
2022-11-30 18:57:58 +0300

    Добавил методы, оформил задачу

--- FinalWork/Program.cs
+++ FinalWork/Program.cs
@@ -6,13 +6,45 @@
 // При решении не рекомендуется пользоваться коллекциями, 
 // лучше обойтись исключительно массивами.
 
+void PrintArray(string[] a)
+{
+    for (int i = 0; i < a.Length; i++)
+    {
+        Console.Write($"{a[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+string[] EditorText(string[] text, int sizeWord)
+{
+    int count = 0;
+    string[] newText = new string[text.Length];
+    for (int i = 0; i < newText.Length; i++)
+    {
+        
+        if (text[i].Length <= sizeWord) 
+        {
+            newText[count] = text[i];
+            count++;
+        }
+    }
+    return newText;
+}
+
+
+
 Console.WriteLine("Введите  ваш текст: ");
 string text = Console.ReadLine();
 
+Console.WriteLine("Введите  максимальную длину строки: ");
+int lengthString = Convert.ToInt32(Console.ReadLine());
+
 string[] textArray = text.Split(" ");
 
-for (int i = 0; i < textArray.Length; i++)
-{
-    if (textArray[i].Length <= 3) Console.Write($"{textArray[i]}, ");
-}
 
+Console.WriteLine("Ваш текст: ");
+PrintArray(textArray);
+string[] editText = EditorText(textArray, lengthString);
+Console.WriteLine("После редакции: ");
+PrintArray(editText);
